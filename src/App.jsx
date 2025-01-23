f4cc5200b48707949f47493deb6faf6422db1bff
Harshad1304 <121097207+Harshad1304@users.noreply.github.com>
2025-01-24 03:21:26 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  
+  return ( 
+    <div className='bg-black h-screen'>
+    <h1 className='font-bold text-2xl underline '>Hello</h1>
+    </div>
+  )
+}
+
+export default App
